72bcf66d1042a8e23741fd570f0c80fbf1fac63b
lynlker <nosrednassel@gmail.com>
2023-08-06 17:58:25 +0200

    add CharacterMenu component

---
+++ src/components/CharacterMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CharacterMenu = () => {
+  // Add code here
+  const characters = [
+    { id: 1, name: 'Anne Marie Lawford', image: '../src/resources/img/anneL.png' },
+    { id: 2, name: 'Character 2', image: 'url_to_character2_image' },
+    // Add more character objects as needed
+  ];
+  
+  return (
+    // JSX code for rendering the character menu
+  );
+};
+
+export default CharacterMenu;
--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -3,7 +3,7 @@ import React from "react";
 const Characters = () => {
   return (
     <div>
-      <h1>Characters</h1>
+      <h1>Characters</h1> 
     </div>
   );
 };
